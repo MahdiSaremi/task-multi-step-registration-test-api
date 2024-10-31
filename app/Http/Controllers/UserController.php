71b26fa94058e81582460eda7ab73c1a1a1fe541
Mahdi Saremi <mahdisarmi00@gmail.com>
2024-10-31 06:52:41 +0330

    add update password and personal info

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -20,6 +21,35 @@ class UserController extends Controller
         $data = $request->validate([
             'email' => 'required|string|email',
             'phone' => ['required', 'string', 'regex:/^(+?98|0)?\d{11}$/'],
+        ]);
+
+        $user->update($data);
+
+        return response()->json(true);
+    }
+
+    public function updatePassword(Request $request)
+    {
+        $user = $this->findUser($request);
+
+        $data = $request->validate([
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user->update([
+            'password' => Hash::make($data['password']),
+        ]);
+
+        return response()->json(true);
+    }
+
+    public function updatePersonalInfo(Request $request)
+    {
+        $user = $this->findUser($request);
+
+        $data = $request->validate([
+            'name' => 'required|string|min:2|max:50',
+            'address' => 'required|string|max:150',
         ]);
 
         $user->update($data);
